8370deb98765b32827135fed48e639b034dab746
doquochao95 <doquochao95@gmail.com>
2022-12-14 11:10:06 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { BaseConfig, NgxScannerQrcodeService, SelectedFiles } from 'ngx-scanner-qrcode';
-import { cambeep_base64 } from './beep.audio';
+import { Component, ViewEncapsulation } from "@angular/core";
+import {
+  BaseConfig,
+  NgxScannerQrcodeService,
+  SelectedFiles,
+} from "ngx-scanner-qrcode";
+import { cambeep_base64 } from "./beep.audio";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
   // To require the rear camera https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
@@ -13,25 +17,25 @@ export class AppComponent {
     isAuto: false,
     isDraw: true,
     isAlwaysEmit: true,
-    text: { font: '25px serif' }, // Hiden { font: '0px', bottom: 40 },
+    text: { font: "25px serif" }, // Hiden { font: '0px', bottom: 40 },
     frame: { lineWidth: 8 },
     medias: {
       audio: false,
       video: {
-        facingMode: 'environment', // Front and back camera { facingMode: front ? "user" : "environment" } 
+        facingMode: "environment", // Front and back camera { facingMode: front ? "user" : "environment" }
         width: { ideal: 1280 },
-        height: { ideal: 720 }
-      }
-    }
+        height: { ideal: 720 },
+      },
+    },
   };
-
+  public filesQtyString: string = "Choose File";
   public selectedFiles: SelectedFiles[] = [];
 
-  constructor(private qrcode: NgxScannerQrcodeService) { }
+  constructor(private qrcode: NgxScannerQrcodeService) {}
 
   public onEvent(e: any): void {
     new Audio(cambeep_base64).play();
-    console.log(e)
+    console.log(e);
   }
 
   public onError(e: any): void {
@@ -43,9 +47,12 @@ export class AppComponent {
   }
 
   public onSelects(files: any) {
-    this.qrcode.loadFiles(files, {...this.config, isDraw: false}).subscribe(res => {
-      this.selectedFiles = res.filter(f => f.urlQR);
-      console.log(res);
-    });
+    this.qrcode
+      .loadFiles(files, { ...this.config, isDraw: false })
+      .subscribe((res) => {
+        this.selectedFiles = res.filter((f) => f.urlQR);
+        console.log(res);
+        res.length == 0 ? this.filesQtyString = "Choose File" :this.filesQtyString = res.length + " files";
+      });
   }
 }
